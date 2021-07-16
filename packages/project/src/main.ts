2a45d6007589deeb3569b3cc75ad1bc25e2f81df
swlws <979623074@qq.com>
2021-07-16 18:00:31 +0800

    feat: 引入element-ui

--- packages/project/src/main.ts
+++ packages/project/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-
+import elementPlus from '~/plugin/element-plus';
 import router from './router';
+import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
+app.use(elementPlus);
 app.mount('#app');
 
 document.title = import.meta.env.VITE_APP_TITLE;
