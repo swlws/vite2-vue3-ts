f4d0a33cf045394ff759d27678b7339b904446f7
swlws <979623074@qq.com>
2021-07-16 17:33:37 +0800

    refactor(all): 工程结构调整

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-createApp(App).mount('#app');
-
-document.title = import.meta.env.VITE_APP_TITLE;
--- src/types/env.d.ts
+++
@@ -1,5 +0,0 @@
-interface ImportMetaEnv {
-  VITE_APP_TITLE: string
-  VITE_HTTP_BASE_URL: string
-  VITE_HTTP_POWER_BASE_URL: string
-}
