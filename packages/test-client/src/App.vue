f578b8978dfae742ca0f58e44f433a26f671dbbb
swlws <979623074@qq.com>
2021-08-16 16:43:14 +0800

    feat(all): 国际化实现方式修改

--- packages/test-client/src/App.vue
+++ packages/test-client/src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <router-view></router-view>
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/runtime-core';
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    [ElConfigProvider.name]: ElConfigProvider,
+  },
+  data() {
+    return {
+      locale: zhCn,
+    };
+  },
+});
 </script>
 
 <style lang="scss">
--- shared/plugin/element-plus.ts
+++ shared/plugin/element-plus.ts
@@ -3,12 +3,12 @@ import { App } from 'vue';
 import 'element-plus/lib/theme-chalk/index.css';
 import 'element-plus/packages/theme-chalk/src/base.scss';
 
-import lang from 'element-plus/lib/locale/lang/zh-cn';
+// import lang from 'element-plus/lib/locale/lang/zh-cn';
 import 'dayjs/locale/zh-cn';
-import locale from 'element-plus/lib/locale';
+// import locale from 'element-plus/lib/locale';
 
 // 设置语言
-locale.use(lang);
+// locale.use(lang);
 
 import {
   // ElAlert,
